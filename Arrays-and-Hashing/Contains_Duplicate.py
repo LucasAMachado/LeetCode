8433f0db11ae1d2db486ad205e5d43747ab5ec15
LucasAMachado <lucas.machado.professional.com>
2024-01-04 12:18:38 -0500

    Adding Arrays and Hashing

---
+++ Arrays-and-Hashing/Contains_Duplicate.py
@@ -0,0 +1,21 @@
+from ast import List
+
+#Solution Number 1 
+#If the number is in the set then it is a dupe
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        seen = set()
+        for num in nums:
+            if num in seen:
+                return True
+            seen.add(num)
+
+        return False
+
+
+#Solution Number 2
+#If the lenght of the set does not equal the length of the nums arr then there must be dups
+    #Since a set cannot have dups 
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        return (len(set(nums)) == len(nums))
