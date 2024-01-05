6c75ccabbebd6560859623d0b442616f292b9514
LucasAMachado <lucas.machado.professional.com>
2024-01-05 12:18:04 -0500

    made a change to the solution

--- Arrays-and-Hashing/Valid_Anagram.py
+++ Arrays-and-Hashing/Valid_Anagram.py
@@ -1,4 +1,3 @@
-#Solution 1 Hash Map
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
         if len(s) != len(t):
@@ -8,14 +7,13 @@ class Solution:
 
         for i in range(len(s)):
             countS[s[i]] = 1 + countS.get(s[i], 0)
-            countT[s[i]] = 1 + countT.get(s[i], 0)
+            countT[t[i]] = 1 + countT.get(t[i], 0)
 
         for c in countS:
             if countS[c] != countT.get(c, 0):
                 return False
 
         return True
-    
 #Solutoin 2 Sorted
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
