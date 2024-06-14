e32c3252e163d173bd4df88f272b11da050f6e25
LucasAMachado <155488066+LucasAMachado@users.noreply.github.com>
2024-06-14 12:47:55 -0400

    Updated Two Sum

--- Arrays_Hashing/Two_Sum.py
+++ Arrays_Hashing/Two_Sum.py
@@ -21,7 +21,20 @@ class Solution:
                 return [nums_map[complement], i]
             
             nums_map[nums[i]] = i
+
+# Solution 3 -> O(n)
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+
+        nums_map = {}
+
+        for index, num in enumerate(nums):
+            diff = target - num
+
+            if diff in nums_map:
+                return [nums_map[diff], index]
             
+            nums_map[num] = index
 
 def main():
     # Testing
