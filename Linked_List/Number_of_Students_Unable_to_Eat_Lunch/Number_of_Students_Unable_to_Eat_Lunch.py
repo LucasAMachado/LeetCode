81529328a3c3bb795f079ed664f0446ea0a3f511
LucasAMachado <155488066+LucasAMachado@users.noreply.github.com>
2024-07-15 16:07:04 -0400

    Number of students unable to eat lunch, solutoin 2

--- Linked_List/Number_of_Students_Unable_to_Eat_Lunch/Number_of_Students_Unable_to_Eat_Lunch.py
+++ Linked_List/Number_of_Students_Unable_to_Eat_Lunch/Number_of_Students_Unable_to_Eat_Lunch.py
@@ -1,26 +1,42 @@
 from typing import List
-
+from typing import Counter
 # Solutoin 1 -> Time Complexity : O(n) : Space Complexity : O(n)
+# class Solution:
+#     def countStudents(self, students: List[int], sandwiches: List[int]) -> int:
+#         res = len(students)
+
+#         counter = {}  # hash map : sandwiche : count
+
+#         # Fill in our hash map with the sandwiche and the
+#         # Count of the number of students who want each time of Sandwhich
+#         for student in students:
+#             if student not in counter:
+#                 counter[student] = 1
+#             else:
+#                 counter[student] += 1
+
+#         for sandwiche in sandwiches:
+#             if sandwiche in counter and counter[sandwiche] > 0:
+#                 res -= 1
+#                 counter[sandwiche] -= 1
+#             else:
+#                 # No sandwiche that the students want
+#                 return res
+
+#         return res
+
+
+# Solution 2 -> Time Complexity : O(n) : Space Complextiy : O(n)
 class Solution:
     def countStudents(self, students: List[int], sandwiches: List[int]) -> int:
         res = len(students)
+        ctr = Counter(students)
 
-        counter = {}  # hash map : sandwiche : count
-
-        # Fill in our hash map with the sandwiche and the
-        # Count of the number of students who want each time of Sandwhich
-        for student in students:
-            if student not in counter:
-                counter[student] = 1
-            else:
-                counter[student] += 1
-
-        for sandwiche in sandwiches:
-            if sandwiche in counter and counter[sandwiche] > 0:
+        for s in sandwiches:
+            if ctr[s] > 0:
                 res -= 1
-                counter[sandwiche] -= 1
+                ctr[s] -= 1
             else:
-                # No sandwiche that the students want
                 return res
 
         return res
