3bc3ff606fe936e2dcf18ee4f91aaac2e6d0a162
LucasAMachado <155488066+LucasAMachado@users.noreply.github.com>
2024-07-17 11:55:31 -0400

    Created Add Two Numbers

---
+++ Linked_List/Add_Two_Numbers/Add_Two_Numbers.py
@@ -0,0 +1,12 @@
+from collections import Optional
+
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
+class Solution:
+    def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
+        ...
