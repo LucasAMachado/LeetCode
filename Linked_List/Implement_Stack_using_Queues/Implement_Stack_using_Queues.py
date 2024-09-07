06832568f480180cd8c907fe1df35c703bf2384c
LucasAMachado <lucas.machado.professional@gmail.com>
2024-09-07 17:35:14 -0400

    Solved using a linked list for practice with linked lists still works on leetcode to do it this way

--- Linked_List/Implement_Stack_using_Queues/Implement_Stack_using_Queues.py
+++ Linked_List/Implement_Stack_using_Queues/Implement_Stack_using_Queues.py
@@ -25,6 +25,53 @@ class MyStack:
     def empty(self) -> bool:
         return len(self.q) == 0
 
+# (Not what supposed to use a linked list but I have done it for practice), Solution 2 -> Time Complexity : O(n) for all operations, Space Complexity : O(n)
+class ListNode:
+    def __init__(self, value, next=None):
+        self.value = value
+        self.next = None
+
+
+class MyQueue:
+
+    def __init__(self):
+        self.head = None
+        self.tail = None
+        self.size = 0
+
+    def push(self, x: int) -> None:
+        newNode = ListNode(x)
+
+        if self.empty():
+            self.head = self.tail = newNode
+        else:
+            self.tail.next = newNode
+            self.tail = newNode
+
+        self.size += 1
+
+    def pop(self) -> int:
+        if self.empty():
+            return None
+
+        headvValue = self.head.value
+
+        self.head = self.head.next
+        self.size -= 1
+
+        return headvValue
+
+    def peek(self) -> int:
+        if self.empty():
+            return None
+
+        return self.head.value
+
+    def empty(self) -> bool:
+        return self.size == 0
+
+
+
 # Your MyStack object will be instantiated and called as such:
 # obj = MyStack()
 # obj.push(x)
